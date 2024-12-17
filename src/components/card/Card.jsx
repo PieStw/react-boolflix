bc46de96a68dff0f5bc085c76dbac9e19c8c1d78
PieStw <piero.stalteri@icloud.com>
2024-12-17 13:02:53 +0100

    milestone 4

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,32 +1,62 @@
 import React from "react";
+import styles from "../../assets/css/Card.module.css";
 
 export default function Card({ bool, element }) {
+  const updateVoto = element.vote_average / 2;
+
+  const renderStars = (rating) => {
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < rating) stars.push("piena");
+      else stars.push("vuota");
+    }
+    return stars;
+  };
+
   return (
     <>
       {bool && (
-        <div className="card">
-          <div className="card-body">
-            <img
-              src={`https://image.tmdb.org/t/p/w342${element.backdrop_path}`}
-            ></img>
-            <h4 className="card-title">{element.title}</h4>
-            <h5 className="card-title">{element.original_title}</h5>
-            <p className="card-text">
-              {element.original_language}
-              <img
-                src={`https://flagsapi.com/${element.original_language.toUpperCase()}/flat/64.png`}
-              />
-            </p>
-            <p className="card-text">{element.vote_average}</p>
+        <div className={styles.card}>
+          <img
+            className={styles.img}
+            src={`https://image.tmdb.org/t/p/w500${element.poster_path}`}
+          ></img>
+          <div className={styles.overlay}>
+            <div className={styles.overlayContainer}>
+              <h4 className="card-title">Titolo: {element.title}</h4>
+              <h6 className="card-title">
+                Titolo originale: {element.original_title}
+              </h6>
+              <p className={styles.voto}>
+                Voto:
+                {renderStars(updateVoto).map((valore) => {
+                  if (valore === "piena")
+                    return <i class="fa-solid fa-star"></i>;
+                  else return <i class="fa-regular fa-star"></i>;
+                })}
+              </p>
+
+              <p className="card-text">
+                Nazionalità:{" "}
+                <p>
+                  {element.original_language}{" "}
+                  <img
+                    src={`https://flagsapi.com/${element.original_language.toUpperCase()}/flat/64.png`}
+                  />
+                </p>
+              </p>
+              <p className="card-text">Sinossi: {element.overview}</p>
+            </div>
           </div>
         </div>
       )}
       {!bool && (
-        <div className="card">
-          <div className="card-body">
-            <img
-              src={`https://image.tmdb.org/t/p/w342${element.backdrop_path}`}
-            ></img>
+        <div className={styles.card}>
+          <img
+            className={styles.img}
+            src={`https://image.tmdb.org/t/p/w500${element.poster_path}`}
+          ></img>
+          <div className={styles.overlay}>
             <h4 className="card-title">{element.name}</h4>
             <h5 className="card-title">{element.original_name}</h5>
             <p className="card-text">
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,38 +1,40 @@
 import { useNetflixContext } from "../contex/NetflixContext";
 import Card from "../components/card/Card";
+import styles from "../assets/css/HomePage.module.css";
 
 export default function HomePage() {
   const { film, serie } = useNetflixContext();
 
   console.log(film);
-  console.log(serie);
 
   return (
     <>
-      <h1>FILM</h1>
+      <h1 className={styles.filmTitle}>FILM</h1>
 
-      {film.length > 0 ? (
-        film.map((element) => (
-          <div key={element.id}>
-            <Card bool={true} element={element}></Card>
-          </div>
-        ))
-      ) : (
-        <p>Nessun film trovato</p>
-      )}
+      <div className={styles.films}>
+        {film.length > 0 ? (
+          film.map((element) => (
+            <div key={element.id}>
+              <Card bool={true} element={element}></Card>
+            </div>
+          ))
+        ) : (
+          <p>Nessun film trovato</p>
+        )}
+      </div>
 
-      <hr />
-      <h1>SERIE TV</h1>
-
-      {serie.length > 0 ? (
-        serie.map((element) => (
-          <div key={element.id}>
-            <Card bool={false} element={element}></Card>
-          </div>
-        ))
-      ) : (
-        <p>Nessun film trovato</p>
-      )}
+      <h1 className={styles.serieTitle}>SERIE TV</h1>
+      <div className={styles.series}>
+        {serie.length > 0 ? (
+          serie.map((element) => (
+            <div key={element.id}>
+              <Card bool={false} element={element}></Card>
+            </div>
+          ))
+        ) : (
+          <p>Nessun film trovato</p>
+        )}
+      </div>
     </>
   );
 }
